762e9e1517721d8727f4cfe8a14625029fd92759
Mencrypto <60235584+Mencrypto@users.noreply.github.com>
2025-11-21 19:42:49 +0000

    Se crea un controlador y una vista para desplegar el título de las preguntas en el home de la página de acuerdo con la configuración de las rutas

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Question extends Model
 {
     /** @use HasFactory<\Database\Factories\QuestionFactory> */
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+         return $this->belongsTo(User::class);
+    }
 }
