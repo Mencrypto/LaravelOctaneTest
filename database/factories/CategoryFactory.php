4b8be26fce5dc26d2641d89e19a7e49291ff1f3e
Mencrypto <60235584+Mencrypto@users.noreply.github.com>
2025-11-25 18:55:12 +0000

    Se corrige problema de color de categoría en el factory para que al autocrear la base de datos lo haga con colores hexadecimales y no textos

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => fake()->word(),
-            'color' =>fake()->paragraph(),
+            'color' =>fake()->hexColor(),
         ];
     }
 }
